829271f593acfb24758c61cf536f0b1da6f36382
ZVLU <zvluwork@gmail.com>
2020-02-01 23:55:11 -0500

    Final Commit

--- Develop/index.js
+++ Develop/index.js
@@ -1,18 +1,70 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
 const axios = require("axios");
+const htmlpdf = require("puppeteer");
+const util = require("util");
+const renderhtml = require("./generateHTML.js");
+const writeFileAsync = util.promisify(fs.writeFile);
 
 
-const questions = [
-    
+function promptUser(){
+    return inquirer.prompt([
+        {
+            type: "list",
+            name: "color",
+            message: "fav color",
+            choices: ["green", "blue", "pink", "red"]
+        },
+        {
+            type: "input",
+            name: "username",
+            message: "What is your GitHub username?"
+        },
+      
+        
+        
+    ]);
+}
 
-  
-];
 
-function writeToFile(fileName, data) {
+
+async function generateHTML() {
+    try {
+       
+
+        let answers = await promptUser();
+        const url = `https://api.github.com/users/${answers.username}`;
+        let res  = await axios.get(url);
+        let stars = await axios.get(url + "/starred"); 
+      
+        const html = renderhtml.generateHTML(answers,res,stars);
+
+        await writeFileAsync(`index.html`,html);
+        
+        await writePDF("index.html",html);
+        writePDF();
+        console.log(res);
+
+        
+      
+} catch(err) {
+    console.log(err);
+}
+
+
+
 
 }
 
-function init() {
-
-init();
+ async function writePDF(){
+     const browser = await htmlpdf.launch({headless: true, slowMo: 150});
+     const page = await browser.newPage();
+     const readHTML = fs.readFileSync("index.html","utf8");
+     await page.setContent(readHTML);
+     await page.pdf({
+         path: "bio.pdf",
+     });
+     console.log("Created");
+     await browser.close();
+ }
+generateHTML();
